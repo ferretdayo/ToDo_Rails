2e935aa76a77fccb9e8af387c61c538850ce6396
ferretdayo <ferretdayo@homail.co.jp>
2016-07-06 16:22:09 +0900

    loginページまでは作れた

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	root 'application#index'
+	root 'users#index'
+  get 'login'   => 'sessions#new'
+  get 'signup'  => 'users#new'
 end
