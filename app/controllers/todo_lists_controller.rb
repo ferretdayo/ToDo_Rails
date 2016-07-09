0ba61f5a438cbc4c057de96c4d68669b61a91362
ferretdayo <ferretdayo@hotmail.co.jp>
2016-07-09 07:44:38 +0000

    todolists表示でcreated_atを追加

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -4,7 +4,6 @@ class TodoListsController < ApplicationController
   # GET /todo_lists
   # GET /todo_lists.json
   def index
-    p current_user.id
     @todo_lists = TodoList.where(:user_id => current_user.id)
   end
 
@@ -27,7 +26,7 @@ class TodoListsController < ApplicationController
   def create
     @todo_list = TodoList.new(todo_list_params)
     @todo_list.user_id = current_user.id
-
+		
     respond_to do |format|
       if @todo_list.save
         format.html { redirect_to @todo_list, notice: 'Todo list was successfully created.' }
